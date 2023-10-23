adf1d6386ff8eb4e507d3d8c03613e58010581f9
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-23 18:05:42 +0300

    Initial Commit

---
+++ MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Yunus Emre Taşdemir on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
